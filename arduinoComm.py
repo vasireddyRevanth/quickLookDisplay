f73982d4fff5e6a06419ed4c4bef482ca64e9e74
vasireddyRevanth <revanth.pc.vasireddy@gmail.com>
2024-05-27 18:45:10 +0530

    Fixing GUI

--- arduinoComm.py
+++ arduinoComm.py
@@ -1,8 +1,10 @@
 import serial
 import re
-
+import cProfile
+import threading
+import time
 # Connects to the Arduino.
-def connect_arduino(port='/dev/ttyUSB1', baud_rate=9600):    
+def connect_arduino(port='/dev/ttyUSB0', baud_rate=9600):    
     try:
         ser = serial.Serial(port, baud_rate)
         return ser
@@ -22,12 +24,21 @@ def read_raw_data(ser):
     if ser is not None:
         try:
             while True:
-                if ser.in_waiting >0:
-                    return ser.readline();
+                if ser.in_waiting >= 26:
+                    # time.sleep(0.1);
+                    print(ser.readline())
+                    return str(ser.readline())
         except Exception as e:
             print((f"Failed to read data: {e}"));
 
+def read_buffer():
+    ser = connect_arduino()
+    for i in range(10):            
+        ser.readline()        
+        print(ser.in_waiting)
+
 def echoMeasured():
+
     try:
         values = extract_messages();
         measured_values = values[3:]
@@ -35,30 +46,12 @@ def echoMeasured():
     except:
         pass
 
-def extract_messages():
-    ser = connect_arduino()
-    arduino_recieved_raw = read_raw_data(ser)
-    
-    try:
-        # Assuming read_raw_data already returns a string suitable for regex
-        string_pattern = r'([^#]*)#'
-        formatted_str_list = re.findall(string_pattern, arduino_recieved_raw)
-        
-        if formatted_str_list:
-            formatted_string = formatted_str_list[0]
-            values = [int(formatted_string[i:i+4].strip()) for i in range(0, len(formatted_string), 4)]
-            return values
-        else:
-            print("No formatted string found.")
-            return []
-    except Exception as e:
-        print(f"Message format not found. Failed to extract data: {e}")
-        return []
-
 
 def extract_messages():
     ser = connect_arduino();
-    arduino_recieved_raw = str(read_raw_data(ser));
+    arduino_recieved_raw = str(read_raw_data(ser))
+
+
     try:
         string_pattern = r'!([^#]*)#'
         formatted_str_list = re.findall(string_pattern, arduino_recieved_raw);
@@ -67,4 +60,8 @@ def extract_messages():
         return values;
     except Exception as e:
         # Handle case where $ or # is not found
+        pass
         print(f"Message format not found. Failed to extract data: {e}")
+
+ser = connect_arduino()
+threading.Thread(target=read_raw_data(ser)).start()
---
+++ newGui.py
@@ -0,0 +1,252 @@
+import random
+import tkinter as tk
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+from arduinoComm import echoMeasured
+from datetime import datetime
+import threading
+
+import random
+from itertools import count
+
+import math
+
+
+##################################
+
+root = tk.Tk();
+root.title("Quick Look Display");
+
+ROOT_HEIGHT = root.winfo_height()
+ROOT_WIDTH = root.winfo_width()
+UPDATE_RATE =100
+
+root.minsize(1000,1000)
+
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+main_frame = tk.Frame(root, bg="red")
+main_frame.grid(sticky="nsew")
+
+main_frame.rowconfigure(0, weight =1);
+main_frame.columnconfigure(0, weight=1);
+main_frame.columnconfigure(1,weight=2);
+
+left_frame = tk.Frame(main_frame, bg="green");
+right_frame = tk.Frame(main_frame, bg="blue");
+
+left_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10);
+right_frame.grid(row=0, column=1, sticky="nsew", padx=10, pady=10);
+
+
+# Quadrants
+right_frame.rowconfigure(0, weight=1)
+right_frame.rowconfigure(1, weight=1)
+right_frame.columnconfigure(0, weight=1)
+right_frame.columnconfigure(1, weight=1)
+
+
+set1_label = tk.Label(left_frame, text="Set X:", bg="green", foreground="white")
+set1_label.grid(row=1, column=1, sticky="w")
+set1_entry = tk.Entry(left_frame)
+set1_entry.grid(row=1, column=2 )
+
+set2_label = tk.Label(left_frame, text="Set X:", bg="green", foreground="white")
+set2_label.grid(row=2, column=1, sticky="w")
+set2_entry = tk.Entry(left_frame)
+set2_entry.grid(row=2, column=2 )
+
+set3_label = tk.Label(left_frame, text="Set X:", bg="green", foreground="white")
+set3_label.grid(row=3, column=1, sticky="w")
+set3_entry = tk.Entry(left_frame)
+set3_entry.grid(row=3, column=2 )
+
+set_button = tk.Button(left_frame, text="Submit")
+set_button.grid(row=5, column=2 , columnspan=2)
+
+measureX_label = tk.Label(left_frame, text="")
+measureX_label.grid(row=1, column=4, padx="10")
+
+measureY_label = tk.Label(left_frame, text="")
+measureY_label.grid(row=2, column=4, padx="10")
+
+measureZ_label = tk.Label(left_frame, text="")
+measureZ_label.grid(row=3, column=4, padx="10")
+
+measured= [];
+
+plt.style.use('bmh')
+# values for first graph
+x = []
+y1 = []
+y2 = []
+y3 = []
+index = count()
+index2 = count()
+
+
+# values for first graph
+x_vals = []
+y_vals = []
+y_vals2 = []
+
+start_time = datetime.now()
+time_elapsed = int()
+def updateElapsedTime():
+    global time_elapsed
+    # Calculate the elapsed time
+    elapsed_time = int((datetime.now() - start_time).total_seconds())
+    
+    # Print the elapsed time
+    print(f"Elapsed time: {elapsed_time}")
+    
+    # Wait for 1 second before checking again
+    time_elapsed = elapsed_time;
+
+# Start a new thread to update the elapsed time
+threading.Thread(target=updateElapsedTime, daemon=True).start()
+
+
+x = [0]
+y1= [0]
+y2 = [0]
+y3 = [0]
+
+
+
+def animate(i):
+    measured = echoMeasured()
+
+    # Generate values
+    x_vals.append(next(index))
+    y_vals.append(math.log(measured[1]))
+    y_vals2.append(measured[2])
+    # Get all axes of figure
+    ax1, ax2 = plt.gcf().get_axes()
+    # Clear current data
+    ax1.cla()
+    ax2.cla()
+    # Plot new data
+    ax1.plot(x_vals, y_vals)
+    ax2.plot(x_vals, y_vals2)
+    
+canvas = FigureCanvasTkAgg(plt.gcf(), master=right_frame)
+canvas.get_tk_widget().grid(column=0, row=1)
+# Create two subplots in row 1 and column 1, 2
+plt.gcf().subplots(1, 2)
+ani = FuncAnimation(plt.gcf(), animate, interval=100, blit=False, save_count=100)
+
+
+
+
+
+root.mainloop();
+##################################
+
+"""
+
+def updateMeasured():
+    measured = echoMeasured();
+    measureX_label.config(text=measured[0]);
+    measureY_label.config(text=measured[1]);
+    measureZ_label.config(text=measured[2]);
+
+
+plt.style.use('bmh')
+x = [0]
+y1= [0]
+y2 = [0]
+y3 = [0]
+start_time = datetime.now()
+time_elapsed = int()
+def updateElapsedTime():
+    global time_elapsed
+    # Calculate the elapsed time
+    elapsed_time = int((datetime.now() - start_time).total_seconds())
+    
+    # Print the elapsed time
+    print(f"Elapsed time: {elapsed_time}")
+    
+    # Wait for 1 second before checking again
+    time_elapsed = elapsed_time;
+# Start a new thread to update the elapsed time
+threading.Thread(target=updateElapsedTime, daemon=True).start()
+def animate():
+    # Generate values
+    x.append(time_elapsed)
+    y1.append(measured[0])
+    y2.append(measured[1])
+    y3.append(measured[2])
+    # Get all axes of figure
+    ax1, ax2, ax3 = plt.gcf().get_axes()
+    # Clear current data
+    ax1.cla()
+    ax2.cla()
+    ax3.cla()
+    # Plot new data
+    ax1.plot(x, y1)
+    ax2.plot(x, y2)
+    ax3.plot(x, y3)
+canvas = FigureCanvasTkAgg(plt.gcf(), master=right_frame)
+canvas.get_tk_widget().grid(column=0, row=1)
+# Create two subplots in row 1 and column 1, 2
+plt.gcf().subplots(1, 2)
+
+# UPDATES
+
+is_scheduled = True
+def schedule_updates():
+    schedule_duration=UPDATE_RATE; # in milliseconds
+
+    global is_scheduled
+    if is_scheduled:
+        updateMeasured();
+        ig();
+        root.after(schedule_duration, schedule_updates);
+
+ani = FuncAnimation(plt.gcf(), animate, interval=100, blit=False)
+plt.show()
+
+schedule_updates()
+root.mainloop()
+
+
+
+
+
+
+index = count()
+index2 = count()
+
+
+
+    canvas = FigureCanvasTkAgg(plt.gcf(),master = right_frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    toolbar = NavigationToolbar2Tk(canvas,root)
+    toolbar.update()
+    canvas.get_tk_widget().pack()
+
+    # graph 1
+    # canvas = FigureCanvasTkAgg(plt.gcf(), master=root)
+    # canvas.get_tk_widget().grid(column=0, row=1)
+    # Create two subplots in row 1 and column 1, 2
+    plt.gcf().subplots(1, 2, 3)
+    ani = FuncAnimation(plt.gcf(), animate, interval=ROOT_HEIGHT, blit=False)
+
+
+# GUI
+root = Tk.Tk()
+label = Tk.Label(root, text="Realtime Animated Graphs")
+
+
+Tk.mainloop()
+
+
+# for i in plt.style.available:
+    # print(i)
+"""
